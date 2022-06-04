d9a897c193db980c53d075a2d98e9f73331f4bae
Faisal <faisalhalimzai@gmail.com>
2022-06-04 20:09:07 +0300

    added new stars array and printed to the console!

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,17 @@
 public class Game {
 
-    public void playGame(){
+    public static void playGame(){
         System.out.println("-------Welcome to Hangman!---------");
+        String word = "Airplane";
+        char[] stars = new char[word.length()];
+        for (int i = 0; i <word.length() ; i++) {
+            System.out.println("*");
+
+        }
+        System.out.println("The word is " + word);
+
     }
+
+
+
 }
