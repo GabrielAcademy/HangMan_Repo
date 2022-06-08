a4da52e025adf83ca1949bcc89157d2d6b357d59
GabrielAcademy <gabriel.corlan@academiaprogramatorilor.ro>
2022-06-08 20:56:47 +0300

    Add verification for letter and while loop

--- src/Game.java
+++ src/Game.java
@@ -12,11 +12,24 @@ public class Game {
         Arrays.fill(stars, '*');
         System.out.println(String.valueOf(stars));
         Scanner sc = new Scanner(System.in);
-        System.out.print("Insert a letter: ");
-        char letter = sc.next().charAt(0);
-        for (int i = 0; i < word.length(); i++) {
-            if (word.charAt(i) == letter){
-                stars[i]=letter;
+
+        while (true) {
+            System.out.print("Insert a letter: ");
+            char letter = sc.next().charAt(0);
+            boolean letterFound = false;
+
+            for (int i = 0; i < word.length(); i++) {
+                if (letter == word.charAt(i)) {
+                    stars[i] = letter;
+                    letterFound = true;
+                }
+            }
+
+            if (letterFound) {
+                System.out.println("You guessed! The word is: " + String.valueOf(stars));
+            }
+            else {
+                System.out.println("You're wrong!");
             }
         }
     }
